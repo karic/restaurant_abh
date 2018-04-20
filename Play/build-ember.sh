e137b52fbe2862f6a8cb44e4bc7c75bd51196fb8
karic <karicvim@gmail.com>
2018-04-20 11:21:53 +0200

    Working Docker deployment for STAGE3

--- Play/build-ember.sh
+++ Play/build-ember.sh
@@ -1,5 +1,5 @@
 cd ember/restaurant_abh
 ember build -prod
-rm -rf ../../public/ember/*
-mv dist/ ../../public/ember
+rm -r -f ../../public/ember/
+mv dist/ ../../public/ember/
 mv ../../public/ember/index.html ../../app/views/index.scala.html
